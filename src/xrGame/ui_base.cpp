3c85df456b17f2594381f3178c19ec4823e980d9
AlistairFox <100073771+AlistairFox@users.noreply.github.com>
2024-03-10 17:05:10 +1000

    оффнул 4х3 соотношение сторон

--- src/xrGame/ui_base.cpp
+++ src/xrGame/ui_base.cpp
@@ -254,7 +254,8 @@ void ui_core::RenderFont()
 
 bool ui_core::is_widescreen()
 {
-	return (Device.dwWidth)/float(Device.dwHeight) > (UI_BASE_WIDTH/UI_BASE_HEIGHT +0.01f);
+	return true; // Only 16x9 offsets
+	//return (Device.dwWidth)/float(Device.dwHeight) > (UI_BASE_WIDTH/UI_BASE_HEIGHT +0.01f);
 }
 
 float ui_core::get_current_kx()
